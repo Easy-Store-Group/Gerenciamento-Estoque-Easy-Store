17e1f1e700857efe91590f9d40d0427e2ccaded9
Raul <bissoliraul97@gmail.com>
2026-06-02 14:27:35 -0300

    feat: create cliente table with migration and update model imports

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,6 +2,7 @@ from app.models import categoria
 from app.models import usuario
 from app.models import produto
 from app.models import movimentacao
+from app.models import cliente
 
 #python -m alembic revision --autogenerate -m "criar tabelas cliente e vendas"
 
