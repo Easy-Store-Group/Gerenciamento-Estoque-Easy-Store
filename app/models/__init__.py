dff64711241638c48ed887b68e5949d7e6297fd0
Guilherme <gui.rzamb@gmail.com>
2026-06-02 11:26:19 -0300

    fix: erros e recriando a tabela

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,8 @@
 from app.models import categoria
 from app.models import usuario
 from app.models import produto
-from app.models import venda
-from app.models import item_venda
-from app.models import conquista
+from app.models import movimentacao
+
+#python -m alembic revision --autogenerate -m "criar tabelas cliente e vendas"
+
+#python -m alembic upgrade head
